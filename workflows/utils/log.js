60e6967c1a8137546e9dd9af230f1959b78d290b
cavinHuang <943866180@qq.com>
2024-07-31 11:33:19 +0800

    feat: 增加日志文件

--- workflows/utils/log.js
+++ workflows/utils/log.js
@@ -1,5 +1,5 @@
 const winston = require("winston");
-
+const DailyRotateFile = require("winston-daily-rotate-file");
 const path = require("path");
 
 const logFile = path.resolve(__dirname, "../../logs/combined.log");
@@ -9,10 +9,17 @@ const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
   transports: [
-    new winston.transports.Console({
-      format: winston.format.simple()
-    }),
-    new winston.transports.File({ filename: logFile })
+    // new winston.transports.Console({
+    //   format: winston.format.simple()
+    // }),
+    // new winston.transports.File({ filename: logFile })
+    new DailyRotateFile({
+      filename: logFile,
+      datePattern: "YYYY-MM-DD",
+      zippedArchive: true,
+      maxSize: "20m",
+      maxFiles: "14d"
+    })
   ]
 });
 
